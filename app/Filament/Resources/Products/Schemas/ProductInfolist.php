af5701856f76e40285271f826f5e204a04e8a95e
sopyanbadawi <sopyanbadawi@gmail.com>
2026-04-12 01:39:56 +0700

    JS 7.3 Praktikum

--- app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -7,6 +7,8 @@ use Filament\Schemas\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Components\ImageEntry;
 use Filament\Infolists\Components\IconEntry;
+use Filament\Schemas\Components\Tabs;
+use Filament\Schemas\Components\Tabs\Tab;
 
 class ProductInfolist
 {
@@ -14,6 +16,61 @@ class ProductInfolist
     {
         return $schema
             ->components([
+                Tabs::make('Product Tabs')
+                ->tabs([
+                    Tab::make('Product Details')
+                        ->schema([
+                            TextEntry::make('name')
+                                ->label('Product Name')
+                                ->weight('bold')
+                                ->color('primary'),
+                            TextEntry::make('id')
+                                ->label('Product ID'),
+                            TextEntry::make('sku')
+                                ->label('Product SKU')
+                                ->badge()
+                                ->color('success'),
+                            TextEntry::make('description')
+                                ->label('Product Description'),
+                            TextEntry::make('created_at')
+                                ->label('Product Creation Date')
+                                ->date('d M Y')
+                                ->color('info'),
+                        ]),
+                    Tab::make('Product Price and Stock')
+                        ->schema([
+                            TextEntry::make('price')                            
+                                ->label('Product Price')
+                                ->weight('bold')
+                                ->color('primary')
+                                ->icon('heroicon-s-currency-dollar'),
+                            TextEntry::make('stock')
+                                ->label('Product Stock'),
+                        ]),
+                    Tab::make('Image and Status')
+                        ->schema([
+                            ImageEntry::make('image')
+                                ->label('Product Image')
+                                ->disk('public'),
+                            TextEntry::make('price')
+                                ->label('Product Price')
+                                ->weight('bold')
+                                ->color('primary')
+                                ->icon('heroicon-s-currency-dollar'),
+                            TextEntry::make('stock')
+                                ->label('Product Stock')
+                                ->weight('bold')
+                                ->color('primary'),
+                            IconEntry::make('is_active')
+                                ->label('Is Active?')
+                                ->boolean(),
+                            IconEntry::make('is_featured')
+                                ->label('Is Featured?')
+                                ->boolean(),
+                        ]),
+                ])
+                ->columnSpanFull()
+                ->vertical(),
                 Section::make('Product Info')
                     ->description('')
                     ->schema([
